9eddf481f5c99a80e1d9411f2430c0663ef0e64c
dwojciec <didier.wojciechowski@gmail.com>
2018-12-16 17:58:00 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,22 @@
 #!/bin/bash
-/opt/acestream/acestreamengine --stream-source
+/opt/acestream/acestreamengine --client-console --login root
+--password root
+--live-cache-type
+memory
+--vod-cache-type
+memory
+--stats-report-interval
+1
+--stats-report-peers
+--slots-manager-use-cpu-limit
+1
+--allow-user-config
+--debug-client-transporter
+--debug-profile
+--log-file
+acestream.log
+--log-debug
+355
+--log-max-size
+20000000
+--debug-filter-aircas
